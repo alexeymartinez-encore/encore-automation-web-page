1b518a30c8794640858222427bd57714ac76609c
alexeymartinez-encore <amartinez@encoreautomation.com>
2024-06-17 13:44:27 -0400

    nav links update

--- src/components/Header/NavigationLinks.jsx
+++ src/components/Header/NavigationLinks.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 export default function NavigationLinks() {
   const navStyle = ({ isActive }) =>
     isActive
-      ? "text-blue-600 my-2 md:my-0  md:mx-2 text-xl transform duration-300"
-      : "hover:text-blue-600 my-2 md:my-0  md:mx-2 text-xl transform duration-300";
+      ? "text-blue-600 my-2 md:my-0  md:mx-2 text-xl transform duration-300 font-thin"
+      : "hover:text-blue-600 my-2 md:my-0  md:mx-2 text-xl transform duration-300 font-thin";
 
   return (
     <>
