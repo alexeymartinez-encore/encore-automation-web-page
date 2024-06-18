8c2a360d7e5fdebb2adb992952e46d1b5f7259c0
alexeymartinez-encore <amartinez@encoreautomation.com>
2024-06-18 12:36:36 -0400

    Footer changes and some images added With some new Automotive Systems

--- src/components/Systems/Automotive/SurfacePrep/AutomotiveFinishInspection.jsx
+++ src/components/Systems/Automotive/SurfacePrep/AutomotiveFinishInspection.jsx
@@ -2,6 +2,7 @@ import SystemDescription from "./SystemDescription";
 import scannerImg from "../../../../assets/automotive/scanner.jpeg";
 import defectDetectionImg from "../../../../assets/automotive/micro-epsilon-sensor.jpg";
 import eagleEye from "../../../../assets/automotive/eagle-eye.webp";
+import automatedDefectMarking from "../../../../assets/automotive/automated-defect-marking.png";
 
 export default function AutomotiveFinishInspection() {
   return (
@@ -32,16 +33,26 @@ export default function AutomotiveFinishInspection() {
       <SystemDescription
         img={eagleEye}
         title="Eagle Eye Top Coat Inspection"
-        desc="Encore's defect detection system uses a Micro-Epsilon reflectCONTROL sensor, based on phase
-         measuring deflectometry, for robotic defect detection on the full exterior surface of automotive 
-         vehicles and their components. Our fully robotic defect detection systems offer multiple configurations 
-         based on production rate and vehicle size, with model-specific simulations ensuring 100% coverage with 
-         minimal overlap. The signal evaluation is optimized for detecting paint defects smaller than 0.3 mm, 
-         and the system automatically recognizes defects on all colors. Additionally, Encore offers an optional 
-         automatic defect marking system to apply marking material at detected defect locations. These fully 
-         robotic defect marking systems also feature multiple configurations based on production rate and vehicle 
-         size and can be located downstream from the defect detection system with line tracking capability. 
-         The system includes an applicator and fluid delivery system for the marking material."
+        desc="Encore Automation's partner, J3D Vision, specializes in implementing in-line inspection systems for
+        100% production, focusing on unconventional inspections and automating visual inspection processes for 
+        automatic feedback. Their primary application is inspecting paint defects on car bodies. J3D Vision has 
+        developed original technology that inspects moving car bodies without line modifications, adapting to 
+        high production speeds up to 150 jobs per hour, with potential for higher speeds. This technology can 
+        inspect the entire vehicle, including edges and characteristic lines, and detect parts out of position, 
+        unaffected by vibration or conveyor changes. With complete industrialization, J3D Vision has gained 
+        significant experience and patented their technology in Spain, with a pending patent in Europe. 
+        Their inspection technology is adaptable to various surfaces, parts, or conditions."
+      />
+      <SystemDescription
+        img={automatedDefectMarking}
+        title="Eagle Eye Top Coat Inspection"
+        desc="The Automated Defect Marking Cell is an advanced system designed to enhance manufacturing quality 
+        control by precisely identifying and marking defects. It features automatic marking of each defect, 
+        ensuring that every detected flaw is accurately highlighted. The system employs advanced algorithms 
+        to automatically generate optimal robotic paths, facilitating efficient and precise defect marking. 
+        Additionally, it uses polish to mark defect locations, providing clear and visible indications for 
+        further inspection or rework. This innovative cell streamlines the defect marking process, significantly 
+        improving overall efficiency and product quality in manufacturing environments."
       />
     </div>
   );
--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import encoreOffice from "../../assets/encore-office.png";
 
 export default function Footer() {
   return (
@@ -76,19 +77,35 @@ export default function Footer() {
             Terms
           </Link>
         </div>
-        <div className="flex flex-col ">
-          <h1 className="py-1">Any Inquiries or Concerns?</h1>
-          <p className="font-thin text-sm">
-            Let us know your email and we'll get in touch with you
-          </p>
-          <div className="flex py-5">
-            <input
-              placeholder="Enter your email."
-              className="bg-gray-800 rounded px-2"
-            />
-            <button className="mx-2 bg-blue-500 md:bg-transparent hover:bg-blue-500  text-white font-light hover:text-white md:my-0 py-2 md:py-2 px-3 md:px-5 border border-blue-500 hover:border-transparent rounded transition duration-300">
-              Submit
-            </button>
+        <div className="flex flex-col">
+          <div>
+            <img className="rounded-sm" src={encoreOffice} />
+            <div className="flex flex-col gap-1 text-gray-100 text-opacity-50 py-1">
+              <Link
+                to="https://www.google.com/maps/place/Encore+Automation+LLC/@42.6375662,-83.2434625,16z/data=!3m1!4b1!4m6!3m5!1s0x8824c07aaa7d1e21:0x129b6e7cc15d299!8m2!3d42.6375623!4d-83.2408876!16s%2Fg%2F1td2vj72?entry=ttu"
+                className="text-xs"
+              >
+                50 Corporate Dr, Auburn Hills, MI 48326
+              </Link>
+              <Link to="tel:+12482530200" className="text-xs">
+                (248)-253-0200
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-col ">
+            <h1 className="py-1">Any Inquiries or Concerns?</h1>
+            <p className="font-thin text-sm">
+              Let us know your email and we'll get in touch with you
+            </p>
+            <div className="flex py-5">
+              <input
+                placeholder="Enter your email."
+                className="bg-gray-800 rounded px-2"
+              />
+              <button className="mx-2 bg-blue-500 md:bg-transparent hover:bg-blue-500  text-white font-light hover:text-white md:my-0 py-2 md:py-2 px-3 md:px-5 border border-blue-500 hover:border-transparent rounded transition duration-300">
+                Submit
+              </button>
+            </div>
           </div>
         </div>
       </div>
